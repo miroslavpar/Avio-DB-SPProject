305e02395e605cd299e0c5f31470c03e6c176cea
Miroslav Parapanov <mparapanov99@gmail.com>
2020-01-05 13:07:56 +0200

    added final commit

---
+++ cmake-build-debug/CMakeFiles/SDPProject.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/miroslavparapanov/workspace/CLion/SDPProject/Invoker.cpp" "/Users/miroslavparapanov/workspace/CLion/SDPProject/cmake-build-debug/CMakeFiles/SDPProject.dir/Invoker.cpp.o"
+  "/Users/miroslavparapanov/workspace/CLion/SDPProject/Plane.cpp" "/Users/miroslavparapanov/workspace/CLion/SDPProject/cmake-build-debug/CMakeFiles/SDPProject.dir/Plane.cpp.o"
+  "/Users/miroslavparapanov/workspace/CLion/SDPProject/dbmanager/TXTPlaneManager.cpp" "/Users/miroslavparapanov/workspace/CLion/SDPProject/cmake-build-debug/CMakeFiles/SDPProject.dir/dbmanager/TXTPlaneManager.cpp.o"
+  "/Users/miroslavparapanov/workspace/CLion/SDPProject/main.cpp" "/Users/miroslavparapanov/workspace/CLion/SDPProject/cmake-build-debug/CMakeFiles/SDPProject.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SDPProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDPProject.dir/main.cpp.o"
+  "CMakeFiles/SDPProject.dir/dbmanager/TXTPlaneManager.cpp.o"
+  "CMakeFiles/SDPProject.dir/Plane.cpp.o"
+  "CMakeFiles/SDPProject.dir/Invoker.cpp.o"
+  "SDPProject.pdb"
+  "SDPProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDPProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
